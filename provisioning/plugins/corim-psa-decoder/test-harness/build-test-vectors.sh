cecd81b1930a0786a8063372f6b5528ece6e8b28
Yogesh Deshpande <yogesh.deshpande@arm.com>
2022-11-11 07:32:20 -0500

    Tidy up on the test generation script

--- provisioning/plugins/corim-psa-decoder/test-harness/build-test-vectors.sh
+++ provisioning/plugins/corim-psa-decoder/test-harness/build-test-vectors.sh
@@ -5,8 +5,31 @@
 set -eu
 set -o pipefail
 
-CORIM_CCA_TEMPLATE=corimMini.json
+# function generate_go_test_vector constructs CBOR test vector using
+# supplied comid and corim json template and saves them in a file
+# $1 file name for comid json template
+# $2 file name for corim json template
+# $3 a qualifier for each cbor test vector name
+# $4 name of the file where the generated CBOR test vectors are aggregated
+generate_go_test_vector () {
+	echo "generating test vector using $1 $2"
+	cocli comid create -t ${1}.json
+	cocli corim create -m ${1}.cbor -t $2 -o corim${1}.cbor
+	echo "// automatically generated from $t.json" >> ${4}
+	echo "var ${3}${1} = "'`' >> ${4}
+	cat corim${1}.cbor | xxd -p >> ${4}
+	echo '`' >> ${4}
+	gofmt -w ${4}
+}
 
+# CORIM TEMPLATE
+CORIM_TEMPLATE=corimMini.json
+
+# CORIM CCA TEMPLATES
+CORIM_CCA_TEMPLATE=corimCca.json
+CORIM_CCA_TEMPLATE1=corimCcaNoProfile.json
+
+# COMID TEMPLATES
 COMID_TEMPLATES=
 COMID_TEMPLATES="${COMID_TEMPLATES} ComidPsaIakPubOne"
 COMID_TEMPLATES="${COMID_TEMPLATES} ComidPsaIakPubTwo"
@@ -20,42 +43,26 @@ COMID_TEMPLATES="${COMID_TEMPLATES} ComidPsaRefValNoImplID"
 COMID_TEMPLATES="${COMID_TEMPLATES} ComidPsaIakPubNoUeID"
 COMID_TEMPLATES="${COMID_TEMPLATES} ComidPsaIakPubNoImplID"
 
-TV_DOT_GO=${TV_DOT_GO?must be set in the environment.}
-
-printf "package main\n\n" > ${TV_DOT_GO}
-
-generate_cbor () {
-	echo "generating cbor using $1 $2"
-	echo $1
-	echo $2
-	cocli comid create -t ${1}.json
-	cocli corim create -m ${1}.cbor -t $2 -o corim${1}.cbor
-	echo "// automatically generated from $t.json" >> ${TV_DOT_GO}
-	echo "var ${3}${1} = "'`' >> ${TV_DOT_GO}
-	cat corim${1}.cbor | xxd -p >> ${TV_DOT_GO}
-	echo '`' >> ${TV_DOT_GO}
-	gofmt -w ${TV_DOT_GO}
-}
-
-for t in ${COMID_TEMPLATES}
-do
-generate_cbor $t $CORIM_CCA_TEMPLATE "unsignedCorim"
-done
-
-
-CORIM_CCA_TEMPLATE=corimCca.json
-CORIM_CCA_TEMPLATE1=corimCcaNoProfile.json
-
+# COMID CCA TEMPLATES
 COMID_CCA_TEMPLATES=
 COMID_CCA_TEMPLATES="${COMID_CCA_TEMPLATES} ComidCcaRefValOne"
 COMID_CCA_TEMPLATES="${COMID_CCA_TEMPLATES} ComidCcaRefValFour"
 
-for t in ${COMID_CCA_TEMPLATES}
+TV_DOT_GO=${TV_DOT_GO?must be set in the environment.}
+
+printf "package main\n\n" > ${TV_DOT_GO}
+
+for t in ${COMID_TEMPLATES}
 do
-generate_cbor $t $CORIM_CCA_TEMPLATE "unsignedCorim"
+	generate_go_test_vector $t $CORIM_TEMPLATE "unsignedCorim" $TV_DOT_GO
 done
 
 for t in ${COMID_CCA_TEMPLATES}
 do
-generate_cbor $t $CORIM_CCA_TEMPLATE1 "unsignedCorimnoprofile"
+	generate_go_test_vector $t $CORIM_CCA_TEMPLATE "unsignedCorim" $TV_DOT_GO
+done
+
+for t in ${COMID_CCA_TEMPLATES}
+do
+	generate_go_test_vector $t $CORIM_CCA_TEMPLATE1 "unsignedCorimnoprofile" $TV_DOT_GO
 done
