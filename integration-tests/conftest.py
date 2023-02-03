9c588914c113c806f8ba03243a77db8f232c2927
SabreenKaur <sabreenderjitkaur.gurjitsingh@arm.com>
2023-02-03 14:51:48 +0000

    Add bad signage failing test
    Signed-off-by: SabreenKaur <sabreenderjitkaur.gurjitsingh@arm.com>

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -38,6 +38,32 @@ def psa_generate_good_evidence():
     success = subprocess.run(evcli_cmd, shell=True)
     assert success.returncode == 0
 
+@pytest.fixture
+def psa_generate_bad_signature_evidence():
+    go_cmds = """
+              go-psa -p 2 < /test-vectors/verification/json/psa-claims-profile-2-integ.json | xxd -p -r > psa-token.cbor &&
+              go-cose-cli -k $EVCLI_TEMPLATES/ec256.json -a ES256 < ./psa-token.cbor
+    """
+
+    signature = subprocess.run(go_cmds, shell=True, stdout=subprocess.PIPE)
+    assert signature.returncode == 0
+    
+    curr_signature = bytearray(signature.stdout)
+    last_byte = len(curr_signature)-2
+    curr_signature[last_byte] =  curr_signature[last_byte] + 1
+    string = curr_signature.decode("utf").replace("\n", "")
+   
+    bad_signage_cmd = "echo " + string + " | xxd -p -r > /psa-token-bad-signage.cbor"
+    final_cbor = subprocess.run(bad_signage_cmd, shell=True)
+    assert final_cbor.returncode == 0
+
+    clean_cmd = """
+                mv /psa-token-bad-signage.cbor /test-vectors/verification/cbor
+                rm ./psa-token.cbor
+    """
+    clean_up = subprocess.run(clean_cmd, shell=True)
+    assert clean_up.returncode == 0
+
 
 @pytest.fixture
 def psa_generate_bad_swcomp_evidence():
