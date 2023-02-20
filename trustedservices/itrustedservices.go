0fe44cf050b2c5e394dbfd9e11a56edc557d5830
Sergei Trofimov <sergei.trofimov@arm.com>
2023-02-20 11:04:24 +0000

    wip-5

--- trustedservices/itrustedservices.go
+++ trustedservices/itrustedservices.go
@@ -4,12 +4,30 @@ import (
 	"github.com/veraison/services/proto"
 )
 
+
+type EndorsementType int32
+
+const (
+	Unset            EndorsementType = iota
+	ReferenceValue
+	VerificationKey
+)
+
 // Aliasing these here, as they are part of the TrustedServices interface,
 // and we should avoid leaking the implementation detail that they're  actually
 // implemented via protobufs.
-type Endorsement proto.Endorsement
-type AttestationToken proto.AttestationToken
+type Endorsement {
+	Scheme string
+	Type EndorsementType
+
+}
 type AppraisalContext proto.AppraisalContext
+
+type AttestationToken struct {
+	TenantId  int
+	Data      []byte
+	MediaType string
+}
 
 type ITrustedServices interface {
 	GetAttestation(token *AttestationToken) (*AppraisalContext, error)
