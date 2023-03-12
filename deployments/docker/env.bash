e1f41d3bc4e7a1aa57a53aa21f040c7af887621c
Sergei Trofimov <sergei.trofimov@arm.com>
2023-03-12 16:30:58 +0000

    deployments/docker: deployment redesign
    - A separate container is used as the "builder" to compile everything
      and generate the deployment structure. Services run in much smaller
      container that do not have to go toolchain.
    - Service logs and VTS stores reside on docker volumes, independent of
      any container.
    - A frontend script can be used to manage the deployment (it relies on
      yet another docker image).
    - Modify end-to-end script to use the docker deployment.
    - Add a top-level make target to build and start the deployment.
    - Add help message to top-level Makefile
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

---
+++ deployments/docker/env.bash
@@ -0,0 +1,10 @@
+__VERAISON_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+set -a
+source $__VERAISON_DIR/deployment.cfg
+set +a
+
+alias veraison="$__VERAISON_DIR/veraison"
+alias cocli="$__VERAISON_DIR/veraison -- cocli"
+alias evcli="$__VERAISON_DIR/veraison -- evcli"
+alias polcli="$__VERAISON_DIR/veraiosn -- polcli"
