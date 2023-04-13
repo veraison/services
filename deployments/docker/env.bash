a50e5b6459ff6366b05f887260b70519c737b9e3
Sergei Trofimov <sergei.trofimov@arm.com>
2023-04-13 15:10:48 +0100

    deploy/docker: resolve env file dir via realpath
    Use realpath when resolving the file location inside env files. This
    way, they will work correctly when sourced via a symlink.
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- deployments/docker/env.bash
+++ deployments/docker/env.bash
@@ -1,4 +1,4 @@
-__VERAISON_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+__VERAISON_DIR=$( cd -- "$( dirname -- $( realpath -- "${BASH_SOURCE[0]}" ) )" &> /dev/null && pwd )
 
 set -a
 source $__VERAISON_DIR/deployment.cfg
