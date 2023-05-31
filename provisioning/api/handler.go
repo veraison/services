615877bc00c044c13881546dd94cbc17dac141b5
Thomas Fossati <thomas.fossati@arm.com>
2023-05-31 11:24:02 +0200

    add proto files linting
    * address protolint warnings
    * remove leftover proto/param file
    * add protolint to the linters CI actions
    
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

--- provisioning/api/handler.go
+++ provisioning/api/handler.go
@@ -13,19 +13,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/veraison/services/capability"
-	"github.com/veraison/services/config"
 	"github.com/veraison/services/handler"
 	"github.com/veraison/services/plugin"
 	"github.com/veraison/services/proto"
 	"github.com/veraison/services/vtsclient"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type IHandler interface {
 	Submit(c *gin.Context)
-	GetServiceState(c *gin.Context)
 	GetWellKnownProvisioningInfo(c *gin.Context)
 }
 
@@ -57,49 +54,6 @@ type ProvisioningSession struct {
 const (
 	ProvisioningSessionMediaType = "application/vnd.veraison.provisioning-session+json"
 )
-
-func (o *Handler) GetServiceState(c *gin.Context) {
-	state := proto.ServiceState{
-		ServerVersion: config.Version,
-	}
-
-	vtsState, err := o.VTSClient.GetServiceState(context.TODO(), &emptypb.Empty{})
-	if err != nil {
-		ReportProblem(c,
-			http.StatusInternalServerError,
-			fmt.Sprintf("could not retrieve service state: %v", err),
-		)
-		return
-	}
-
-	apiMediaTypeList, err := structpb.NewList([]interface{}{ProvisioningSessionMediaType})
-	if err != nil {
-		panic(err) // Should never happen as the value above is hard-coded.
-	}
-
-	handlerMediaTypeList, err := proto.NewStringList(o.PluginManager.GetRegisteredMediaTypes())
-	if err != nil {
-		ReportProblem(c,
-			http.StatusInternalServerError,
-			fmt.Sprintf("could not retrieve handler media types: %v", err),
-		)
-		return
-	}
-
-	state.SupportedMediaTypes = map[string]*structpb.ListValue{
-		"endorsement-provisioning/v1": apiMediaTypeList,
-		"handler":                     handlerMediaTypeList.AsListValue(),
-	}
-
-	if vtsState.Status == proto.ServiceStatus_DOWN {
-		state.Status = proto.ServiceStatus_INITIALIZING
-	} else {
-		state.Status = proto.ServiceStatus_READY
-	}
-
-	c.Header("Content-Type", proto.ServiceStateMediaType)
-	c.JSON(http.StatusOK, &state)
-}
 
 func (o *Handler) Submit(c *gin.Context) {
 	// read the accept header and make sure that it's compatible with what we
