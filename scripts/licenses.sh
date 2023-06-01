e3ae37f1948e95d70d5a15f60e54e06745220925
Thomas Fossati <thomas.fossati@arm.com>
2023-06-01 13:23:01 +0100

    Add license scan
    Signed-off-by: Thomas Fossati <thomas.fossati@arm.com>

--- scripts/licenses.sh
+++ scripts/licenses.sh
@@ -8,11 +8,16 @@ type go-licenses &> /dev/null || go get github.com/google/go-licenses
 
 MODULES+=(". github.com/veraison/services/...")
 
+function header() {
+  echo "Go package,License file URL,License type"
+}
+
 for module in "${MODULES[@]}"
 do
   dir=$(echo "${module}" | cut -d' ' -f1)
   mod=$(echo "${module}" | cut -d' ' -f2)
 
-  echo ">> retrieving licenses [ ${mod} ]"
+  >&2 echo ">> retrieving licenses [ ${mod} ]"
+  header
   ( cd "${dir}" && go-licenses csv ${mod} )
 done
