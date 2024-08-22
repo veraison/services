2063e7e56950d9b7158b75a7d644f51c4dd2aa54
Thomas Fossati <thomas.fossati@linaro.org>
2024-08-22 16:40:36 +0000

    fix: install go v1.22 when bootstrapping native
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- deployments/native/bootstrap/ubuntu.sh
+++ deployments/native/bootstrap/ubuntu.sh
@@ -2,10 +2,10 @@
 # Copyright 2024 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
 sudo apt update
-sudo apt install --yes git protobuf-compiler golang-1.20 make gettext sqlite3 openssl jq
+sudo apt install --yes git protobuf-compiler golang-1.22 make gettext sqlite3 openssl jq
 
-sudo ln -s /usr/lib/go-1.20/bin/go /usr/local/bin/go
-sudo ln -s /usr/lib/go-1.20/bin/gofmt /usr/local/bin/gofmt
+sudo ln -s /usr/lib/go-1.22/bin/go /usr/local/bin/go
+sudo ln -s /usr/lib/go-1.22/bin/gofmt /usr/local/bin/gofmt
 
 wget https://dl.smallstep.com/cli/docs-cli-install/latest/step-cli_amd64.deb -O /tmp/step-cli_amd64.deb
 sudo dpkg -i /tmp/step-cli_amd64.deb
