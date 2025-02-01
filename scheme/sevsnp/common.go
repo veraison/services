fb9b045517c2c8324ef09e8076626a125d2d86ea
Jagannathan Raman <jag.raman@oracle.com>
2025-02-01 05:44:37 +0100

    feat(sevsnp): get keys from evidence
    Update the store handler to get Trust Anchor and Reference Value
    keys from evidence.
    
    Add helper routines to parse the TSM report's auxblob to extract
    AMD keys.
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

--- scheme/sevsnp/common.go
+++ scheme/sevsnp/common.go
@@ -4,9 +4,19 @@
 package sevsnp
 
 import (
+	"encoding/pem"
+	"errors"
+	"github.com/google/go-sev-guest/abi"
+	"github.com/google/go-sev-guest/proto/sevsnp"
 	"github.com/veraison/corim/comid"
+	"github.com/veraison/corim/corim"
 	"github.com/veraison/ratsd/tokens"
 	"github.com/veraison/services/proto"
+)
+
+var (
+	ErrCertificateReadFailure = errors.New("failed to read certificate")
+	ErrMissingCertChain       = errors.New("evidence missing certificate chain")
 )
 
 // measurementByUintKey looks up comid.Measurement in a CoMID by its MKey.
@@ -43,4 +53,56 @@ func parseEvidence(token *proto.AttestationToken) (*tokens.TSMReport, error) {
 	}
 
 	return &tsm, nil
+}
+
+// comidFromJson accepts a CoRIM in JSON format and returns its first CoMID
+//
+//	Returns error if there are more than a single CoMID, or passes on
+//	error from corim routine.
+func comidFromJson(buf []byte) (*comid.Comid, error) {
+	extractedCorim, err := corim.UnmarshalUnsignedCorimFromJSON(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(extractedCorim.Tags) > 1 {
+		return nil, errors.New("too many tags")
+	}
+
+	extractedComid, err := corim.UnmarshalComidFromCBOR(
+		extractedCorim.Tags[0].Content,
+		extractedCorim.Profile,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return extractedComid, nil
+}
+
+func parseCertificateChainFromEvidence(tsm *tokens.TSMReport) (*sevsnp.CertificateChain, error) {
+	var certTable abi.CertTable
+
+	if len(tsm.AuxBlob) == 0 {
+		return nil, ErrMissingCertChain
+	}
+
+	if err := certTable.Unmarshal(tsm.AuxBlob); err != nil {
+		return nil, err
+	}
+
+	return certTable.Proto(), nil
+}
+
+func readCert(cert []byte) ([]byte, error) {
+	if len(cert) == 0 {
+		return nil, errors.New("empty certificate")
+	}
+
+	block, _ := pem.Decode(cert)
+	if block == nil || block.Type != "CERTIFICATE" {
+		return nil, ErrCertificateReadFailure
+	}
+	return block.Bytes, nil
 }
