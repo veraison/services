f60a9e00a9e5e03a90c530883d2d0117ffefc004
Jagannathan Raman <jag.raman@oracle.com>
2025-02-01 04:29:32 +0100

    feat(scheme): create plugin for AMD SEV-SNP
    define SEV-SNP scheme for Veraison.
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

---
+++ scheme/sevsnp/scheme.go
@@ -0,0 +1,22 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+package sevsnp
+
+const (
+	SchemeName             = "SEVSNP"
+	EndorsementMediaTypeRV = `application/corim-unsigned+cbor; profile="tag:amd.com,2024:snp-corim-profile"`
+	// ToDo: check media type for AMD ARK
+	EndorsementMediaTypeTA = `application/corim-unsigned+cbor; profile="https://amd.com/ark"`
+	EvidenceMediaType      = "application/vnd.veraison.tsm-report+cbor"
+)
+
+var (
+	EndorsementMediaTypes = []string{
+		EndorsementMediaTypeRV,
+		EndorsementMediaTypeTA,
+	}
+
+	EvidenceMediaTypes = []string{
+		EvidenceMediaType,
+	}
+)
