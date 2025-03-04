f7c87a0590b2c754c4c2e850b7c59d2f86b7ff64
Sergei Trofimov <sergei.trofimov@arm.com>
2025-03-04 13:50:38 +0000

    feat: remove auth from .well-known API
    Allow getting .well-known data from provisioning and management services
    without needing to authenticate.
    
    The API is read-only, and there is nothing confidential being returned,
    so there is no real reason why it needs to be authenticated. Removing
    auth allows using .well-known for heath checks.
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- management/api/router.go
+++ management/api/router.go
@@ -1,38 +1,49 @@
-// Copyright 2023 Contributors to the Veraison project.
+// Copyright 2023-2025 Contributors to the Veraison project.
 // SPDX-License-Identifier: Apache-2.0
-
 package api
 
 import (
+	"path"
+
 	"github.com/gin-gonic/gin"
 	"github.com/veraison/services/auth"
 )
 
-var publicApiMap = map[string]string{
-	"createPolicy":       "/management/v1/policy/:scheme",
-	"activatePolicy":     "/management/v1/policy/:scheme/:uuid/activate",
-	"getActivePolicy":    "/management/v1/policy/:scheme",
-	"getPolicy":          "/management/v1/policy/:scheme/:uuid",
-	"deactivatePolicies": "/management/v1/policies/:scheme/deactivate",
-	"getPolicies":        "/management/v1/policies/:scheme",
-}
+const (
+	managementPath = "/management/v1"
+)
+
+var publicApiMap = make(map[string]string)
 
 func NewRouter(handler Handler, authorizer auth.IAuthorizer) *gin.Engine {
 	router := gin.New()
 
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	router.Use(authorizer.GetGinHandler(auth.ManagerRole))
-
-	router.POST(publicApiMap["createPolicy"], handler.CreatePolicy)
-	router.POST(publicApiMap["activatePolicy"], handler.Activate)
-	router.GET(publicApiMap["getActivePolicy"], handler.GetActivePolicy)
-	router.GET(publicApiMap["getPolicy"], handler.GetPolicy)
-
-	router.POST(publicApiMap["deactivatePolicies"], handler.DeactivateAll)
-	router.GET(publicApiMap["getPolicies"], handler.GetPolicies)
 
 	router.GET("/.well-known/veraison/management", handler.GetManagementWellKnownInfo)
+
+	manageGroup := router.Group(managementPath)
+	manageGroup.Use(authorizer.GetGinHandler(auth.ManagerRole))
+
+	manageGroup.POST("policy/:scheme", handler.CreatePolicy)
+	publicApiMap["createPolicy"] = path.Join(managementPath, "policy/:scheme")
+
+	manageGroup.POST("policy/:scheme/:uuid/activate", handler.Activate)
+	publicApiMap["activatePolicy"] = path.Join(managementPath, "policy/:scheme/:uuid/activate")
+
+	manageGroup.GET("policy/:scheme", handler.GetActivePolicy)
+	publicApiMap["getActivePolicy"] = path.Join(managementPath, "policy/:scheme")
+
+	manageGroup.GET("policy/:scheme/:uuid", handler.GetPolicy)
+	publicApiMap["getPolicy"] = path.Join(managementPath, "policy/:scheme/:uuid")
+
+	manageGroup.POST("policies/:scheme/deactivate", handler.DeactivateAll)
+	publicApiMap["deactivatePolicies"] = path.Join(managementPath,
+						"policies/:scheme/deactivate")
+
+	manageGroup.GET("policies/:scheme", handler.GetPolicies)
+	publicApiMap["getPolicies"] = path.Join(managementPath, "policies/:scheme")
 
 	return router
 }
