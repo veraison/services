ca89661f9e1444946a8e59a115f3a6252d7d34ac
Sergei Trofimov <sergei.trofimov@arm.com>
2025-03-05 12:06:01 +0000

    chore(vts): refactor EarSigner
    - Separate key acquisition from the rest of signer init by offloading
      the former to the new KeyLoader
    - KeyLoader loads the key as raw bytes from a URL, selecting the backend
      based on its scheme. For backward compatibility, URLs that do not
      specify a scheme are treated as if the scheme was "file".
    - Existing file loading code is factored out into FileKeyLoader
      (currently, the only backend).
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- vts/earsigner/README.md
+++ vts/earsigner/README.md
@@ -4,5 +4,8 @@
   result signing process.  The supported directives are:
   - `alg`: the [JWS algorithm](https://www.iana.org/assignments/jose/jose.xhtml#web-signature-encryption-algorithms)
     used for signing, e.g.: `ES256`, `RS512`.
-  - `key`: file containing the private key to be used with `alg`.
+  - `key`: URL with the location of the private key to be used with `alg`. The
+    following URL schems are supported:
+    - `file`: URL path is the path to a local file
+    If a scheme is not specified, `file` is assumed.
     The key is in [JWK format](https://datatracker.ietf.org/doc/rfc7517/).
---
+++ vts/earsigner/keyloader.go
@@ -0,0 +1,39 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+package earsigner
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/spf13/afero"
+)
+
+type KeyLoader struct {
+	loaders map[string]IKeyLoader
+}
+
+func NewKeyLoader(fs afero.Fs) *KeyLoader {
+	return &KeyLoader{
+		loaders: map[string]IKeyLoader{
+			"file": NewFileKeyLoader(fs),
+		},
+	}
+}
+
+func (o KeyLoader) Load(location *url.URL) ([]byte, error) {
+	var scheme string
+
+	if location.Scheme == "" {
+		scheme = "file"
+	} else {
+		scheme =  location.Scheme
+	}
+
+	actualLoader, ok := o.loaders[scheme]
+	if !ok {
+		return nil, fmt.Errorf("invalid key loader scheme: %s", scheme)
+	}
+
+	return actualLoader.Load(location)
+}
