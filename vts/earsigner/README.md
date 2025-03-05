9507a5ad226cac0b82692b58c38e06f10d07e2eb
Sergei Trofimov <sergei.trofimov@arm.com>
2025-03-05 12:53:51 +0000

    feat(vts): add EAR signer key loader for AWS
    Add AwsKeyLoader for Ear signer, that loads the key from the AWS Secrets
    Manager, using the default AWS SDK credentials.
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- vts/earsigner/README.md
+++ vts/earsigner/README.md
@@ -7,5 +7,8 @@
   - `key`: URL with the location of the private key to be used with `alg`. The
     following URL schems are supported:
     - `file`: URL path is the path to a local file
+    - `aws`: URL path is in the form `<region>:<secret-name>` where `<region>`
+      is an AWS region, and `<secrete-name>` is the name under which the key is
+      stored in AWS Secrets Manager.
     If a scheme is not specified, `file` is assumed.
     The key is in [JWK format](https://datatracker.ietf.org/doc/rfc7517/).
---
+++ vts/earsigner/keyloader_aws.go
@@ -0,0 +1,55 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+package earsigner
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+// AwsKeyLoader is IKeyLoader implementation that loads the key from AWS Secrets Manager.
+type AwsKeyLoader struct {
+	context context.Context
+}
+
+// NewAwsKeyLoader creates a new NewAwsKeyLoader using the specified Context.
+func NewAwsKeyLoader(c context.Context) *AwsKeyLoader {
+	return &AwsKeyLoader{c}
+}
+
+// Load the key from the specified URL. The url must be in the following format:
+//    aws:<region>/<secret-name>
+// Where <region> is the AWS region (e.g. eu-west-1), and <secret-name> is the
+// name under which the key is stored in the AWS Secrets Manager
+func (o AwsKeyLoader) Load(location *url.URL) ([]byte, error) {
+	parts := strings.Split(location.Opaque, "/")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("invalid AWS key location: %s", location.Opaque)
+	}
+	region, name := parts[0], parts[1]
+
+	config, err := config.LoadDefaultConfig(o.context, config.WithRegion(region))
+	if err != nil {
+		return nil, err
+	}
+
+	client := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(name),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	resp, err := client.GetSecretValue(o.context, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(*resp.SecretString), nil
+}
