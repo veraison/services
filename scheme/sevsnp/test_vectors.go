94631b13797b8c541846ec3d13fab8c4cb5e3cf8
Jagannathan Raman <jag.raman@oracle.com>
2025-03-06 14:35:51 -0500

    sevsnp tests: add unit tests
    Add unit tests for endorsement, evidence and storage handlers
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

---
+++ scheme/sevsnp/test_vectors.go
@@ -0,0 +1,10 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+package sevsnp
+
+import _ "embed"
+
+var (
+	//go:embed test/corim/unsignedCorimSevSnp.cbor
+	unsignedCorimSevSnp []byte
+)
