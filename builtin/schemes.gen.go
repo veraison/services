f128356f6030ce0c88eadd8ae7ffab2452560d49
Thomas Fossati <thomas.fossati@linaro.org>
2025-04-10 13:10:15 +0200

    chore: go fmt store_rpc.go and schemes.gen.go
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- builtin/schemes.gen.go
+++ builtin/schemes.gen.go
@@ -1,33 +1,32 @@
 package builtin
 
 import (
-        "github.com/veraison/services/plugin"
+	"github.com/veraison/services/plugin"
 
-        scheme1 "github.com/veraison/services/scheme/parsec-cca"
-        scheme2 "github.com/veraison/services/scheme/riot"
-        scheme3 "github.com/veraison/services/scheme/arm-cca"
-        scheme4 "github.com/veraison/services/scheme/tpm-enacttrust"
-        scheme5 "github.com/veraison/services/scheme/parsec-tpm"
-        scheme6 "github.com/veraison/services/scheme/psa-iot"
-
+	scheme3 "github.com/veraison/services/scheme/arm-cca"
+	scheme1 "github.com/veraison/services/scheme/parsec-cca"
+	scheme5 "github.com/veraison/services/scheme/parsec-tpm"
+	scheme6 "github.com/veraison/services/scheme/psa-iot"
+	scheme2 "github.com/veraison/services/scheme/riot"
+	scheme4 "github.com/veraison/services/scheme/tpm-enacttrust"
 )
 
 var plugins = []plugin.IPluggable{
-        &scheme1.EvidenceHandler{},
-        &scheme1.EndorsementHandler{},
-        &scheme1.StoreHandler{},
-        &scheme2.EvidenceHandler{},
-        &scheme2.StoreHandler{},
-        &scheme3.EvidenceHandler{},
-        &scheme3.EndorsementHandler{},
-        &scheme3.StoreHandler{},
-        &scheme4.EvidenceHandler{},
-        &scheme4.EndorsementHandler{},
-        &scheme4.StoreHandler{},
-        &scheme5.EvidenceHandler{},
-        &scheme5.EndorsementHandler{},
-        &scheme5.StoreHandler{},
-        &scheme6.EvidenceHandler{},
-        &scheme6.EndorsementHandler{},
-        &scheme6.StoreHandler{},
+	&scheme1.EvidenceHandler{},
+	&scheme1.EndorsementHandler{},
+	&scheme1.StoreHandler{},
+	&scheme2.EvidenceHandler{},
+	&scheme2.StoreHandler{},
+	&scheme3.EvidenceHandler{},
+	&scheme3.EndorsementHandler{},
+	&scheme3.StoreHandler{},
+	&scheme4.EvidenceHandler{},
+	&scheme4.EndorsementHandler{},
+	&scheme4.StoreHandler{},
+	&scheme5.EvidenceHandler{},
+	&scheme5.EndorsementHandler{},
+	&scheme5.StoreHandler{},
+	&scheme6.EvidenceHandler{},
+	&scheme6.EndorsementHandler{},
+	&scheme6.StoreHandler{},
 }
