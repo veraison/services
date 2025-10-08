21d4ba1696b9ffb61f2f180d255b36754f9f2d9e
Kallal Mukherjee <ritamukherje62@gmail.com>
2025-10-08 09:45:45 +0000

    fix(vts): fix shebang position and variable quoting in init-kvstores.sh
    - Move shebang to first line to fix SC1128 error
    - Add proper quoting to variable in sqlite3 command to fix SC2086 error
    
    Addresses shellcheck violations found during bash script review.
    
    Signed-off-by: Kallal Mukherjee <ritamukherje62@gmail.com>

--- vts/test-harness/init-kvstores.sh
+++ vts/test-harness/init-kvstores.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 # Copyright 2022-2023 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
-#
-#!/bin/bash
 
 set -eux
 set -o pipefail
@@ -9,5 +8,5 @@ set -o pipefail
 for t in en ta po
 do
     echo "CREATE TABLE kvstore ( key text NOT NULL, vals text NOT NULL );" | \
-        sqlite3 $t-store.sql
+        sqlite3 "${t}-store.sql"
 done
