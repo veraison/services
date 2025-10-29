0bfdf6bb6f3b2b853a3569adbe2eb49f548a68e3
Thomas Fossati <thomas.fossati@linaro.org>
2025-10-29 12:27:30 +0100

    remove Init method from ICoservSigner interface
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- vts/coservsigner/icoservsigner.go
+++ vts/coservsigner/icoservsigner.go
@@ -5,12 +5,10 @@ package coservsigner
 import (
 	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
-	"github.com/spf13/afero"
 	"github.com/veraison/corim/coserv"
 )
 
 type ICoservSigner interface {
-	Init(cfg Cfg, fs afero.Fs) error
 	Sign(coserv coserv.Coserv) ([]byte, error)
 	GetCoservSigningPublicKey() (jwa.KeyAlgorithm, jwk.Key, error)
 	Close() error
