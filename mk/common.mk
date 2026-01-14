dae0d8fe7d220411981a8ae0b0d3761eeb6af840
setrofim <setrofim@gmail.com>
2026-01-14 14:21:25 +0000

    fix(make): move TOPDIR into mk/common.mk
    Move the definition of TOPDIR from the top-level Makefile into
    mk/common.mk. This ensures that the definition is available when
    invoking a sub-Makefile directly (e.g. make -C vts/cmd/vts-service).
    
    This, in turn, makes sure the pat to get-veraison-version script gets
    resolved correctly, and the version gets embedded into the executable.
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- mk/common.mk
+++ mk/common.mk
@@ -1,10 +1,11 @@
-# Copyright 2021 Contributors to the Veraison project.
+# Copyright 2021-2026 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
 
 SHELL := /bin/bash
 
 # Pass this to sub-make
 export GO111MODULE := on
+export TOPDIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 # Used to set the ServerVersion reported by  services
 VERSION_FROM_GIT := $(shell $(TOPDIR)/scripts/get-veraison-version)
