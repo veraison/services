e3249f8c3776ffb4027bba543364952aaedea8ca
Yogesh Deshpande <yogesh.deshpande@arm.com>
2026-03-06 18:52:06 +0000

    Add scheme settings
    Signed-off-by: Yogesh Deshpande <yogesh.deshpande@arm.com>

--- scheme/ratsd/scheme.go
+++ scheme/ratsd/scheme.go
@@ -1,3 +1,29 @@
 // Copyright 2023-2026 Contributors to the Veraison project.
 // SPDX-License-Identifier: Apache-2.0
 package ratsd
+
+import (
+	"github.com/veraison/services/handler"
+	"github.com/veraison/services/log"
+	"go.uber.org/zap"
+)
+
+var Descriptor = handler.SchemeDescriptor{
+	Name:          "RATSD",
+	VersionMajor:  1,
+	VersionMinor:  0,
+	CorimProfiles: []string{""},
+	EvidenceMediaTypes: []string{
+		`application/cmw-collection+cbor; eat_profile="tag:github.com,2025:veraison/ratsd/cmw"`,
+	},
+}
+
+type Implementation struct {
+	logger *zap.SugaredLogger
+}
+
+func NewImplementation() *Implementation {
+	return &Implementation{
+		logger: log.Named(Descriptor.Name),
+	}
+}
