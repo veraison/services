526b453e1faa8e755adfd08e22f79bd1150b8b5d
Yogesh Deshpande <yogesh.deshpande@arm.com>
2026-03-09 16:09:29 +0000

    Add scheme dunamics
    Signed-off-by: Yogesh Deshpande <yogesh.deshpande@arm.com>

--- scheme/ratsd/scheme.go
+++ scheme/ratsd/scheme.go
@@ -3,8 +3,11 @@
 package ratsd
 
 import (
+	"github.com/veraison/corim/comid"
+	"github.com/veraison/ear"
 	"github.com/veraison/services/handler"
 	"github.com/veraison/services/log"
+	"github.com/veraison/services/vts/appraisal"
 	"go.uber.org/zap"
 )
 
@@ -26,4 +29,46 @@ func NewImplementation() *Implementation {
 	return &Implementation{
 		logger: log.Named(Descriptor.Name),
 	}
+}
+
+func (o *Implementation) GetTrustAnchorIDs(
+	evidence *appraisal.Evidence,
+) ([]*comid.Environment, error) {
+	return nil, nil // TODO
+}
+
+func (o *Implementation) GetReferenceValueIDs(
+	trustAnchors []*comid.KeyTriple,
+	claims map[string]any,
+) ([]*comid.Environment, error) {
+	return nil, nil // TODO
+}
+
+func (o *Implementation) ValidateComid(c *comid.Comid) error {
+	return nil // TODO
+}
+
+func (o *Implementation) ExtractClaims(
+	evidence *appraisal.Evidence,
+	trustAnchors []*comid.KeyTriple,
+) (map[string]any, error) {
+	return nil, nil // TODO
+}
+
+func (o *Implementation) ValidateEvidenceIntegrity(
+	evidence *appraisal.Evidence,
+	trustAnchors []*comid.KeyTriple,
+	endorsements []*comid.ValueTriple,
+) error {
+	return nil // TODO
+}
+
+func (o *Implementation) AppraiseClaims(
+	claims map[string]any,
+	endorsements []*comid.ValueTriple,
+) (*ear.AttestationResult, error) {
+	result := handler.CreateAttestationResult(Descriptor.Name)
+	//appraisal := result.Submods[Descriptor.Name]
+	// TODO
+	return result, nil
 }
