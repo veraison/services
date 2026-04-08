1859154292609433256baf8ffcda6d8246ed1efb
Sergei Trofimov <sergei.trofimov@arm.com>
2026-04-08 12:08:15 +0100

    build: install golangci-lint as part of lint target
    Install a specific version of golangci-lint as part of the lint Makefile
    target, and use that version for linting.
    
    There are often incompatible changes from version to version of
    golangci-lint. Some linters become deprecated, replaced by new onces.
    Occasionally, .golangci.yml format changes.
    
    This means we often need to deal with unrelated linter issues before we
    can submit pull requests. Moreover, different projects may expect
    different linter versions, further necessitating upgrading/downgrading
    of the system-installed binary.
    
    To get around this, install a repo-local binary of specific version
    automatically as a precondition to running the lint Makefile target. The
    version can then be upgrading in a controlled way, independent of other
    changes, without impacting other projects.
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- mk/lint.mk
+++ mk/lint.mk
@@ -1,18 +1,26 @@
-# Copyright 2021 Contributors to the Veraison project.
+# Copyright 2021-2026 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
 #
 # variables:
-# * GOLINT - name of the linter package
 # * GOLINT_ARGS - command line arguments for $(LINT) when run on the lint target
 # targets:
 # * lint  - run source code linter
 
-GOLINT ?= golangci-lint
-
 GOLINT_ARGS ?= run --timeout=3m -E dupl -E gocritic -E gosimple -E prealloc
 
+GOLINT_VERSION = v1.64.8
+GOLINT = $(TOPDIR)/tools-bin/golangci-lint
+GOLINT_STAMP = $(TOPDIR)/tools-bin/golangci-lint-$(GOLINT_VERSION).stamp
+
+$(GOLINT): $(GOLINT_STAMP)
+
+$(GOLINT_STAMP):
+	mkdir -p $(dir $(GOLINT))
+	touch $(GOLINT_STAMP)
+	GOBIN=$(dir $(GOLINT)) go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLINT_VERSION)
+
 .PHONY: lint
-lint: lint-hook-pre reallint
+lint: $(GOLINT) lint-hook-pre reallint
 
 .PHONY: lint-hook-pre
 lint-hook-pre:
