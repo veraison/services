a2b25d3d181893338158c7c4864e7587abe23bfe
Kumar, Atul <Atul.Kumar@fujitsu.com>
2026-04-20 14:03:14 +0530

    feat: implement endorsements lifecycle mgmt. API
    This PR implements the REST API endpoints and handlers for activation
    and deactivation of endorsement triples as specified by the spec
    (See
    https://github.com/veraison/docs/blob/main/api/endorsement-provisioning/endorsement-provisioning.yaml).
    
    - update some copyrights
    - use application/concise-problem-details+cbor
    - add integration-tests
    
    Signed-off-by: Kumar, Atul <Atul.Kumar@fujitsu.com>

--- provisioning/api/handler.go
+++ provisioning/api/handler.go
@@ -23,6 +23,7 @@ var (
 
 type IHandler interface {
 	Submit(c *gin.Context)
+	SetActive(setActive bool) gin.HandlerFunc
 	GetWellKnownProvisioningInfo(c *gin.Context)
 }
 
@@ -41,9 +42,9 @@ func NewHandler(
 	maxPayloadSize int64,
 ) IHandler {
 	return &Handler{
-		Provisioner:   p,
-		logger:        logger,
-		WkCacheMaxAge: capability.ParseCacheMaxAge(wkCacheMaxAge, defaultCacheMaxAge, logger),
+		Provisioner:    p,
+		logger:         logger,
+		WkCacheMaxAge:  capability.ParseCacheMaxAge(wkCacheMaxAge, defaultCacheMaxAge, logger),
 		MaxPayloadSize: maxPayloadSize,
 	}
 }
@@ -56,6 +57,7 @@ type ProvisioningSession struct {
 
 const (
 	ProvisioningSessionMediaType = "application/vnd.veraison.provisioning-session+json"
+	ELMQueryMediaType            = "application/vnd.veraison.elm-v1+cbor"
 )
 
 func (o *Handler) Submit(c *gin.Context) {
@@ -125,7 +127,7 @@ func (o *Handler) Submit(c *gin.Context) {
 	if err != nil {
 		o.logger.Errorw("submit endorsement failed", "error", err)
 
-		if errors.Is(err, errors.New("no connection")) {
+		if strings.Contains(err.Error(), "no connection") {
 			ReportProblem(c,
 				http.StatusInternalServerError,
 				err.Error(),
@@ -141,6 +143,65 @@ func (o *Handler) Submit(c *gin.Context) {
 	}
 
 	sendSuccessfulProvisioningSession(c)
+}
+
+func (o *Handler) SetActive(setActive bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// read media type
+		mediaType := c.Request.Header.Get("Content-Type")
+		if mediaType != ELMQueryMediaType {
+			ReportConciseProblem(
+				c,
+				http.StatusUnsupportedMediaType,
+				fmt.Sprintf("unknown media type: %v, expected: %v", mediaType, ELMQueryMediaType),
+			)
+			return
+		}
+
+		// read body
+		payload, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			ReportConciseProblem(c,
+				http.StatusBadRequest,
+				fmt.Sprintf("error reading body: %s", err),
+			)
+			return
+		}
+
+		if len(payload) == 0 {
+			ReportConciseProblem(c,
+				http.StatusBadRequest,
+				"empty body",
+			)
+			return
+		}
+
+		err = o.Provisioner.SetEndorsementsActive(tenantID, payload, setActive)
+		if err != nil {
+			action := "deactivate"
+			if setActive {
+				action = "activate"
+			}
+
+			o.logger.Errorw(action+" endorsement failed", "error", err)
+
+			if strings.Contains(err.Error(), "no connection") {
+				ReportConciseProblem(c,
+					http.StatusInternalServerError,
+					err.Error(),
+				)
+				return
+			}
+
+			ReportConciseProblem(c,
+				http.StatusBadRequest,
+				err.Error(),
+			)
+			return
+		}
+
+		c.AbortWithStatus(http.StatusNoContent)
+	}
 }
 
 func sendFailedProvisioningSession(c *gin.Context, failureReason string) {
--- provisioning/provisioner/provisioner.go
+++ provisioning/provisioner/provisioner.go
@@ -1,4 +1,4 @@
-// Copyright 2022-2024 Contributors to the Veraison project.
+// Copyright 2022-2026 Contributors to the Veraison project.
 // SPDX-License-Identifier: Apache-2.0
 
 package provisioner
@@ -78,6 +78,26 @@ func (p *Provisioner) SubmitEndorsements(tenantID string, data []byte, mt string
 			sRes.Status.GetErrorDetail(),
 		)
 	}
+	return nil
+}
+
+func (p *Provisioner) SetEndorsementsActive(tenantID string, data []byte, setActive bool) error {
+	aReq := &proto.SetEndorsementsActiveRequest{Data: data, SetActive: setActive}
+	aRes, err := p.VTSClient.SetEndorsementsActive(context.Background(), aReq)
+	if err != nil {
+		if errors.As(err, &vtsclient.NoConnectionError{}) {
+			return errors.New("no connection")
+		}
+		return fmt.Errorf("change endorsements activation state (setting active = %v) failed: %w", setActive, err)
+	}
+
+	if !aRes.GetStatus().Result {
+		return fmt.Errorf(
+			"change endorsements activation state (setting active = %v) failed: %s",
+			setActive, aRes.Status.GetErrorDetail(),
+		)
+	}
+
 	return nil
 }
 
