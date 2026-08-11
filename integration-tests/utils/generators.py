6b1457f076d8668f93e419d88d95bd71cf143a6b
Ian Chin Wang <ian.chin.wang@oracle.com>
2026-08-11 13:18:03 -0400

    Fix base64 padding in integration test nonce conversion
    Signed-off-by: Ian Chin Wang <ian.chin.wang@oracle.com>

--- integration-tests/utils/generators.py
+++ integration-tests/utils/generators.py
@@ -1,7 +1,6 @@
 # Copyright 2023-2026 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
 import ast
-import base64
 import json
 import os
 import shutil
@@ -94,12 +93,11 @@ def generate_artefacts_from_response(response, scheme, evidence, signing, keys, 
 
 def base64url_to_base64(value):
     # evcli claim JSON uses standard base64, while session nonces are base64url.
-	replacer := strings.NewReplacer(
-	    "-", "+",
-	    "_", "/",
-	)
+    translated = value.replace('-', '+').replace('_', '/')
 
-	return replacer.Replace(value)
+    # Session nonces use unpadded base64url, while evcli claim JSON requires
+    # padded standard base64.
+    return translated + '=' * (-len(translated) % 4)
 
 
 def generate_expected_result_from_response(response, scheme, expected):
